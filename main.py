2b7008abdd1eaa06e3c86595fbfd7580b05ac32a
DenysLeonenko <leonenkodendev@gmail.com>
2023-10-16 14:13:33 +0300

    LaboratoryWork 3 improved version

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 input_value = input("Введіть ціле число = ")
+input_char = input("Введіть символ, яким ви хочите виводити діаграму: ")
 
-if not input_value.isdigit():
+if input_value.isdigit():
+    number_array = [int(number) for number in input_value]
+    print(*number_array)
+    print("_ " * len(input_value))
+    max_value = max(number_array)
+    for row_value in range(max_value, 0, -1):
+        row_str = ""
+        for num in number_array:
+            if num >= row_value:
+                row_str += input_char + " "
+            else:
+                row_str += "  "
+        print(row_str)
+else:
     print("Вводиме значення має бути цілим числом")
-    input_value = input("Введіль ціле число, ще раз")
-
-number_array = [int(number) for number in input_value]
-print(*number_array)
-print("_ " * len(input_value))
-max_value = max(number_array)
-for row_value in range(max_value, 0, -1):
-    row_str = ""
-    for num in number_array:
-        if num >= row_value:
-            row_str += "# "
-        else:
-            row_str += "  "
-    print(row_str)
+    input_value = input("Введіль ціле число, ще раз = ")
